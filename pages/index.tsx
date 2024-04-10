11d74fcbead6e26bdbc6e800ea89ed77db77bc51
moustafa elsayed <moelsayed949@gmail.com>
2024-04-11 01:45:10 +0200

    add new packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,101 +1,101 @@
-"use client"
-import React, { useState, useCallback, useEffect } from "react";
-import Select from "react-select";
+import { useCallback, useEffect, useState } from "react";
+import { TextField } from "@mui/material";
+import Autocomplete from "@mui/material/Autocomplete";
 import axios from "axios";
 import { Private_Key, Url } from "@/api";
 
 interface Category {
   id: number;
   name: string;
+  description?: string | null;
+  image: string;
+  slug: string;
   children?: Category[] | null;
+  circle_icon?: string;
+  disable_shipping: number;
 }
 
 interface SubCatOption {
   id: number;
   name: string;
-  options?: Option[];
+  options: any;
 }
 
-interface Option {
-  id: number;
-  name: string;
+interface MainCategoryData {
+  categories: Category[];
 }
 
-export default function Home(): JSX.Element {
-  const [categories, setCategories] = useState<Category[]>([]);
+interface Props {
+  mainCategoryData: MainCategoryData;
+}
+
+export default function Home({ mainCategoryData }: Props) {
+  // states
   const [subCatData, setSubCatData] = useState<Category[] | null>(null);
   const [subCatOptions, setSubCatOptions] = useState<SubCatOption[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
-  const [selectedSubCategories, setSelectedSubCategories] = useState<Category[]>([]);
-  const [selectedOptions, setSelectedOptions] = useState<SubCatOption[]>([]);
+  const [additionalInputValue, setAdditionalInputValue] = useState("");
 
-  useEffect(() => {
-    async function fetchCategories() {
-      try {
-        const response = await axios.get(`${Url}/get_categories`, {
-          headers: {
-            "private-key": Private_Key,
-          },
-        });
-        const { data } = response.data;
-        setCategories(data.categories);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    }
-
-    fetchCategories();
-  }, []);
+  // callbacks
+  const filterOptions = useCallback(
+    (options: Category[], { inputValue }: { inputValue: string }) =>
+      options.filter((option) =>
+        option.name.toLowerCase().includes(inputValue.toLowerCase())
+      ),
+    []
+  );
 
   const handleAutocompleteChange = useCallback(
-    (value: Category | null) => {
+    (event: React.SyntheticEvent, value: Category | null) => {
       if (value && Array.isArray(value.children)) {
-        setSelectedCategory(value);
         setSubCatData(value.children);
-        setSelectedSubCategories([]);
-        setSelectedOptions([]);
       } else {
-        setSelectedCategory(null);
-        setSubCatData(null);
-        setSelectedSubCategories([]);
-        setSelectedOptions([]);
+        setSubCatData([]);
       }
     },
     []
   );
 
   const handleSubCategoryChange = useCallback(
-    async (value: Category | null) => {
+    (event: React.SyntheticEvent, value: Category | null) => {
+      setSubCatOptions([]);
       if (value) {
-        try {
-          const response = await axios.get<{ data: SubCatOption[] }>(`${Url}/properties?cat=${value.id}`, {
+        console.log(value?.id);
+        axios
+          .get(`${Url}/properties?cat=${value?.id}`, {
             headers: {
               "private-key": Private_Key,
             },
+          })
+          .then((res) => {
+            setSubCatOptions(
+              res?.data?.data.map((category: any) => ({
+                ...category,
+                options: [
+                  ...category.options,
+                  {
+                    id: new Date().getTime(),
+                    name: "other",
+                  },
+                ],
+              }))
+            );
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
           });
-          const { data } = response.data;
-          setSubCatOptions(
-            data.map((category: SubCatOption) => ({
-              ...category,
-              options: [
-                ...category.options,
-                {
-                  id: new Date().getTime(),
-                  name: "other",
-                },
-              ],
-            }))
-          );
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
       } else {
-        console.error("Any error");
+        console.error("any error");
       }
     },
     []
   );
 
+  const handleCatOptionsChange = useCallback(
+    (event: React.SyntheticEvent, value: Category | null) => {
+      console.log("Selected value is ", value);
+    },
+    []
+  );
   const handleSubmit = () => {
     console.log("Submitted!");
     // Handle form submission here
@@ -105,48 +105,79 @@ export default function Home(): JSX.Element {
     <main className="flex min-h-screen flex-row items-start justify-center p-24 gap-5 bg-[#F6F4F5] text-black shadow-2xl h-full">
       <div className="px-4 w-2/4 border border-yellow-400-100 bg-white p-5 flex flex-col justify-center items-center gap-5 rounded-xl">
         <div className="capitalize">Searchable dropdown menu</div>
-
-        <div className="capitalize">Select a category</div>
-        <Select
-          className="w-full"
-          value={selectedCategory}
-          onChange={handleAutocompleteChange}
-          options={categories.map((category) => ({
-            value: category,
-            label: category.name,
-          }))}
-        />
-        {subCatData && (
-          <>
-            <div className="capitalize">Select a sub-category</div>
-            <Select
-              className="w-full"
-              isMulti
-              value={selectedSubCategories}
-              onChange={(options) => setSelectedSubCategories(options)}
-              options={subCatData.map((subcategory) => ({
-                value: subcategory.id,
-                label: subcategory.name,
-                children: subcategory.children,
-              }))}
-            />
-          </>
-        )}
-        {selectedSubCategories.length > 0 && (
-          <>
-            <div className="capitalize">Select options</div>
-            <Select
-              className="w-full"
-              isMulti
-              value={selectedOptions}
-              onChange={setSelectedOptions}
-              options={subCatOptions.map((option) => ({
-                value: option.id,
-                label: option.name,
-              }))}
-            />
-          </>
-        )}
+        <div className="flex flex-col items-center mt-8">
+          <div className="flex items-center mb-4">
+            <label htmlFor="first-movie" className="mr-1 text-black">
+              Select Category
+            </label>
+            <span className="text-red-500">*</span>
+          </div>
+          <Autocomplete
+            disablePortal
+            options={mainCategoryData?.categories}
+            sx={{ width: 300 }}
+            getOptionLabel={(option) => option.name}
+            filterOptions={filterOptions}
+            renderInput={(params) => (
+              <TextField {...params} label="Main Category" />
+            )}
+            onChange={handleAutocompleteChange}
+          />
+          <div className="flex items-center mt-4">
+            <label htmlFor="second-movie" className="mr-1 text-black">
+              Sub Category{" "}
+            </label>
+            <span className="text-red-500">*</span>
+          </div>
+          <Autocomplete
+            disablePortal
+            options={subCatData || []} // Provide an empty array if subCatData is null
+            sx={{ width: 300 }}
+            getOptionLabel={(option) => option.name}
+            filterOptions={filterOptions}
+            renderInput={(params) => (
+              <TextField {...params} label="Sub Category" />
+            )}
+            onChange={handleSubCategoryChange}
+          />
+          <div className="mt-4 mb-8">
+            {subCatOptions.length > 0 && (
+              <>
+                {subCatOptions.map((item) => (
+                  <div className="mt-2" key={item.id}>
+                    <label htmlFor="first-movie" className="mr-1 text-black">
+                      {item?.name}
+                    </label>
+                    <Autocomplete
+                      disablePortal
+                      options={item?.options}
+                      sx={{ width: 300 }}
+                      getOptionLabel={(option) => option.name}
+                      filterOptions={filterOptions}
+                      renderInput={(params) => (
+                        <>
+                          <TextField {...params} />
+                          {params.inputProps?.value === "other" && (
+                            <TextField
+                              id="additional-input"
+                              label="Additional Input"
+                              variant="outlined"
+                              value={additionalInputValue}
+                              onChange={(e) =>
+                                setAdditionalInputValue(e.target.value)
+                              }
+                            />
+                          )}
+                        </>
+                      )}
+                      onChange={handleCatOptionsChange}
+                    />
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+        </div>
         <button
           onClick={handleSubmit}
           className="border border-gray-500 w-1/4 h-10 rounded-xl bg-[#F6F4F5]"
@@ -156,4 +187,26 @@ export default function Home(): JSX.Element {
       </div>
     </main>
   );
+}
+
+export async function getServerSideProps() {
+  try {
+    const GetMainCategoryData = await axios.get(`${Url}/get_all_cats`, {
+      headers: {
+        "private-key": Private_Key,
+      },
+    });
+
+    return {
+      props: {
+        mainCategoryData: GetMainCategoryData?.data?.data,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        apiHeader: null,
+      },
+    };
+  }
 }
